c816e7becebe699782a624c405db55e7a889eb6f
Adhil Salim <bizadhil@gmail.com>
2023-04-21 18:46:38 +0530

    mv assets to src/assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import DashboardPage from "./components/Sidebar/Sidebar";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUnPage from "./pages/SignUpPage/SignUpPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <div className="App">
-      <SignUnPage />
+      <SignUpPage />
+      hello
     </div>
   );
 }
