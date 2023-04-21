206f9f51930d4913023f0ad07303100d0ee748b6
Adhil Salim <bizadhil@gmail.com>
2023-04-21 17:43:31 +0530

    Create singuppage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import DashboardPage from "./components/Sidebar/Sidebar";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
+import SignUnPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
-      <SignInPage />
-      <Sidebar />
+      <SignUnPage />
     </div>
   );
 }
