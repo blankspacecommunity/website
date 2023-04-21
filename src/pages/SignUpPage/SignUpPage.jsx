17a4d09ca0898a0e07d46a414eb55603604950ff
Adhil Salim <bizadhil@gmail.com>
2023-04-21 21:09:50 +0530

    Update SignUpPage.jsx
    toggle button

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -100,9 +100,133 @@ export default function SignUpPage() {
                     </div>
                   </div>
                 </li>
+                <li className="nav-item dropdown">
+                  <a
+                    href="account-signup.html#"
+                    className="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                  >
+                    Pages
+                  </a>
+                  <div className="dropdown-menu">
+                    <div className="d-lg-flex pt-lg-3">
+                      <div className="mega-dropdown-column">
+                        <h6 className="px-3 mb-2">About</h6>
+                        <ul className="list-unstyled mb-3">
+                          <li>
+                            <a
+                              href="about-v1.html"
+                              className="dropdown-item py-1"
+                            >
+                              About v.1
+                            </a>
+                          </li>
+                        </ul>
+                        <h6 className="px-3 mb-2">Blog</h6>
+                        <ul className="list-unstyled mb-3">
+                          <li>
+                            <a
+                              href="blog-podcast.html"
+                              className="dropdown-item py-1"
+                            >
+                              Podcast
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="mega-dropdown-column">
+                        <h6 className="px-3 mb-2">Portfolio</h6>
+                        <ul className="list-unstyled mb-3">
+                          <li>
+                            <a
+                              href="portfolio-grid.html"
+                              className="dropdown-item py-1"
+                            >
+                              Grid View
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="mega-dropdown-column">
+                        <h6 className="px-3 mb-2">Contacts</h6>
+                        <ul className="list-unstyled mb-3">
+                          <li>
+                            <a
+                              href="contacts-v1.html"
+                              className="dropdown-item py-1"
+                            >
+                              Contacts v.1
+                            </a>
+                          </li>
+                        </ul>
+                        <h6 className="px-3 mb-2">Specialty</h6>
+                        <ul className="list-unstyled">
+                          <li>
+                            <a
+                              href="404-v1.html"
+                              className="dropdown-item py-1"
+                            >
+                              404 Error v.1
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    href="account-signup.html#"
+                    className="nav-link dropdown-toggle active"
+                    data-bs-toggle="dropdown"
+                    aria-current="page"
+                  >
+                    Account
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a href="account-details.html" className="dropdown-item">
+                        Account Details
+                      </a>
+                    </li>
+                  </ul>
+                </li>
               </ul>
             </div>
           </div>
+          <div
+            className="form-check form-switch mode-switch pe-lg-1 ms-auto me-4"
+            data-bs-toggle="mode"
+          >
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="theme-mode"
+            />
+            <label
+              className="form-check-label d-none d-sm-block"
+              htmlFor="theme-mode"
+            >
+              Light
+            </label>
+            <label
+              className="form-check-label d-none d-sm-block"
+              htmlFor="theme-mode"
+            >
+              Dark
+            </label>
+          </div>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
       </header>
       <div className="container d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5">
