eab9281d2dd407ce946dd0fda6cce3117a3da0ca
Adhil Salim <bizadhil@gmail.com>
2023-04-21 21:28:49 +0530

    Update PageContent.jsx

--- src/pages/SignUpPage/PageContent.jsx
+++ src/pages/SignUpPage/PageContent.jsx
@@ -5,17 +5,15 @@ import Footer from "./Footer";
 
 export default function PageContent() {
   return (
-    <>
-      <div className="container d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5">
-        <div
-          className="w-100 align-self-end pt-1 pt-md-4 pb-4"
-          style={{ maxWidth: "526px" }}
-        >
-          <FormHeader />
-          <FormContent />
-        </div>
-        <Footer />
+    <div className="container d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5">
+      <div
+        className="w-100 align-self-end pt-1 pt-md-4 pb-4"
+        style={{ maxWidth: "526px" }}
+      >
+        <FormHeader />
+        <FormContent />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
