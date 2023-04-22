17b06e9b53e9c6200e341684124c581a39b41ddb
Akhil <akhillav@gmail.com>
2023-04-22 22:14:52 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import DashboardPage from "./components/Sidebar/Sidebar";
 import LandingPage from "./pages/LandingPage/LandingPage";
@@ -7,7 +8,7 @@ import SignUpPage from "./pages/SignUpPage/SignUpPage";
 function App() {
   return (
     <div className="App">
-      <SignUpPage />
+      <Navbar />
     </div>
   );
 }
