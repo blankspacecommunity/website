945903836fdfaccb42b0c453188e49e93b37feeb
Adhil Salim <bizadhil@gmail.com>
2023-04-26 08:16:19 +0530

    Create PageContent.jsx

---
+++ src/pages/SignInPage/PageContent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import FormContent from "./FormContent";
+
+export default function PageContent() {
+  return (
+    <div className="container d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5">
+      <div
+        className="w-100 align-self-end pt-1 pt-md-4 pb-4"
+        style={{ maxWidth: "526px" }}
+      >
+        <h1 className="text-center text-xl-start">Welcome Back</h1>
+        <p className="text-center text-xl-start pb-3 mb-3">
+          Don’t have an account yet?{" "}
+          <a href="account-signup.html">Register here.</a>
+        </p>
+
+        <FormContent />
+      </div>
+    </div>
+  );
+}
