3bdcfe3daf962496700f0ce471f4d84dbeb82ca5
Adhil Salim <bizadhil@gmail.com>
2023-05-03 12:50:02 +0530

    add toast

--- src/pages/SignUpPage/FormContent.jsx
+++ src/pages/SignUpPage/FormContent.jsx
@@ -1,7 +1,19 @@
 import React, { useRef, useState } from "react";
+import { ToastContainer } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Toast from "react-bootstrap/Toast";
 import { createAccountWithEmailAndPassword } from "../../scripts/firebase/authentication/authentication";
 
 export default function FormContent() {
+  const [showToast, setShowToast] = useState(false);
+  const [toastContent, setToastContent] = useState({
+    title: "",
+    code: "",
+    message: "",
+    delay: 0,
+    position: "top-end",
+  });
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
@@ -56,129 +68,155 @@ export default function FormContent() {
   };
   // why getting eslint error near labels?
   return (
-    <form className="needs-validation" onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col-sm-6">
-          <div className="position-relative mb-4">
-            <label htmlFor="name" className="form-label fs-base">
-              Full name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="form-control form-control-lg"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <div className="invalid-feedback position-absolute start-0 top-100">
-              Please enter your name!
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6">
-          <div className="position-relative mb-4">
-            <label htmlFor="email" className="form-label fs-base">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control form-control-lg"
-              required
-            />
-            <div className="invalid-feedback position-absolute start-0 top-100">
-              Please enter a valid email address!
-            </div>
-          </div>
-        </div>
-        <div className="col-12 mb-4">
-          <div className="position-relative mb-4">
-            <label htmlFor="username" className="form-label fs-base">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="form-control form-control-lg"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-            <div className="invalid-feedback position-absolute start-0 top-100">
-              Please enter your username!
-            </div>
-          </div>
-        </div>
-        <div className="col-12 mb-4">
-          <label htmlFor="password" className="form-label fs-base">
-            Password
-          </label>
-          <div className="password-toggle">
-            <input
-              type="password"
-              id="password"
-              className="form-control form-control-lg"
-              ref={passwordRef}
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label
-              className="password-toggle-btn"
-              aria-label="Show/hide password"
-            >
+    <>
+      <form className="needs-validation" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="position-relative mb-4">
+              <label htmlFor="name" className="form-label fs-base">
+                Full name
+              </label>
               <input
-                className="password-toggle-check"
-                type="checkbox"
-                onClick={(e) => handlePasswordToggle(e, passwordRef)}
+                type="text"
+                id="name"
+                className="form-control form-control-lg"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
               />
-              <span className="password-toggle-indicator" />
-            </label>
-            <div className="invalid-feedback position-absolute start-0 top-100">
-              Please enter a password!
+              <div className="invalid-feedback position-absolute start-0 top-100">
+                Please enter your name!
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-12 mb-4">
-          <label htmlFor="password-confirm" className="form-label fs-base">
-            Confirm password
-          </label>
-          <div className="password-toggle">
-            <input
-              type="password"
-              id="password-confirm"
-              className="form-control form-control-lg"
-              ref={confirmPasswordRef}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-            <label
-              className="password-toggle-btn"
-              aria-label="Show/hide password"
-            >
+          <div className="col-sm-6">
+            <div className="position-relative mb-4">
+              <label htmlFor="email" className="form-label fs-base">
+                Email
+              </label>
               <input
-                className="password-toggle-check"
-                type="checkbox"
-                onClick={(e) => handlePasswordToggle(e, confirmPasswordRef)}
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control form-control-lg"
+                required
               />
-              <span className="password-toggle-indicator" />
+              <div className="invalid-feedback position-absolute start-0 top-100">
+                Please enter a valid email address!
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <div className="position-relative mb-4">
+              <label htmlFor="username" className="form-label fs-base">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control form-control-lg"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <div className="invalid-feedback position-absolute start-0 top-100">
+                Please enter your username!
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <label htmlFor="password" className="form-label fs-base">
+              Password
             </label>
-            <div className="invalid-feedback position-absolute start-0 top-100">
-              Please enter a password!
+            <div className="password-toggle">
+              <input
+                type="password"
+                id="password"
+                className="form-control form-control-lg"
+                ref={passwordRef}
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label
+                className="password-toggle-btn"
+                aria-label="Show/hide password"
+              >
+                <input
+                  className="password-toggle-check"
+                  type="checkbox"
+                  onClick={(e) => handlePasswordToggle(e, passwordRef)}
+                />
+                <span className="password-toggle-indicator" />
+              </label>
+              <div className="invalid-feedback position-absolute start-0 top-100">
+                Please enter a password!
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <label htmlFor="password-confirm" className="form-label fs-base">
+              Confirm password
+            </label>
+            <div className="password-toggle">
+              <input
+                type="password"
+                id="password-confirm"
+                className="form-control form-control-lg"
+                ref={confirmPasswordRef}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+              <label
+                className="password-toggle-btn"
+                aria-label="Show/hide password"
+              >
+                <input
+                  className="password-toggle-check"
+                  type="checkbox"
+                  onClick={(e) => handlePasswordToggle(e, confirmPasswordRef)}
+                />
+                <span className="password-toggle-indicator" />
+              </label>
+              <div className="invalid-feedback position-absolute start-0 top-100">
+                Please enter a password!
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <button
-        type="submit"
-        className="btn btn-primary shadow-primary btn-lg w-100"
-      >
-        Sign up
-      </button>
-    </form>
+        <button
+          type="submit"
+          className="btn btn-primary shadow-primary btn-lg w-100"
+        >
+          Sign up
+        </button>
+      </form>
+      <Row>
+        <Col xs={6}>
+          <ToastContainer position="top-end" className="p-3">
+            <Toast
+              onClose={() => setShowToast(false)}
+              show={showToast}
+              delay={toastContent.delay}
+              position={toastContent.position}
+              autohide
+            >
+              <Toast.Header>
+                <img
+                  src="holder.js/20x20?text=%20"
+                  className="rounded me-2"
+                  alt=""
+                />
+                <strong className="me-auto">{toastContent.title}</strong>
+                <small>{toastContent.code}</small>
+              </Toast.Header>
+              <Toast.Body>{toastContent.message}</Toast.Body>
+            </Toast>
+          </ToastContainer>
+        </Col>
+      </Row>
+    </>
   );
 }
