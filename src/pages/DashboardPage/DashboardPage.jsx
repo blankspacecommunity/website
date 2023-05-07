65b2d27e4b65cbf2fd700fee5b70617a4947d01c
Adhil Salim <bizadhil@gmail.com>
2023-05-07 08:40:21 +0530

    remove nonsense

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,29 +1,22 @@
 import React from "react";
-import { useNavigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import DashboardSidebar from "./DashboardSidebar/DashboardSidebar";
 import { auth } from "../../scripts/firebase/config/firebaseConfig";
 
 export default function DashboardPage() {
   const user = auth.currentUser;
-  const navigate = useNavigate();
 
   return (
     <main className="page-wrapper">
-      {user ? (
-        <>
-          <Navbar />
+      <Navbar />
 
-          <section className="container">
-            <div className="row">
-              <DashboardSidebar />
-              <Outlet />
-            </div>
-          </section>
-        </>
-      ) : (
-        navigate("/signin")
-      )}
+      <section className="container">
+        <div className="row">
+          <DashboardSidebar />
+          <Outlet />
+        </div>
+      </section>
     </main>
   );
 }
