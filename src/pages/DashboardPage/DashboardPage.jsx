9365c2e37314ab2625815955185c9f7ae2b2097d
Adhil Salim <bizadhil@gmail.com>
2023-05-07 08:38:21 +0530

    navigate non auth user

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import DashboardSidebar from "./DashboardSidebar/DashboardSidebar";
+import { auth } from "../../scripts/firebase/config/firebaseConfig";
 
 export default function DashboardPage() {
+  const user = auth.currentUser;
+  const navigate = useNavigate();
+
   return (
     <main className="page-wrapper">
-      <Navbar />
+      {user ? (
+        <>
+          <Navbar />
 
-      <section className="container">
-        <div className="row">
-          <DashboardSidebar />
-          <Outlet />
-        </div>
-      </section>
+          <section className="container">
+            <div className="row">
+              <DashboardSidebar />
+              <Outlet />
+            </div>
+          </section>
+        </>
+      ) : (
+        navigate("/signin")
+      )}
     </main>
   );
 }
