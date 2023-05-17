3a62596f1e3abe3f63b0f25d16728a49bb675332
Adhil Salim <bizadhil@gmail.com>
2023-05-17 16:29:15 +0530

    add /contact

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import "./assets/vendor/boxicons/css/boxicons.min.css"; // CSS for icons
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import ContactPage from "./pages/ContactPage/ContactPage";
 import Error404 from "./pages/Error404/Error404";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import Profile from "./pages/DashboardPage/ProfileSection/Profile";
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: "/resetpassword",
     element: <ResetPassword />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
   },
   {
     path: "/dashboard",
